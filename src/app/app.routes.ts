2d54b37d538d37fd221a50072f70653bec9df0ab
simran-srk <simranreyazkhan7@gmail.com>
2024-07-14 15:33:01 +0530

    Initial setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: NavbarComponent },
+    { path: 'explore', component: CatalogueComponent },
+];
