fbb06f1cb379b12804790ebbeb81c1dbfe9691d6
simran-srk <simranreyazkhan7@gmail.com>
2024-07-14 21:12:06 +0530

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import {RouterModule, Routes } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
-import { CatalogueComponent } from './catalogue/catalogue.component';
-
-export const routes: Routes = [
-    { path: '', component: NavbarComponent },
-    { path: 'explore', component: CatalogueComponent },
-];
